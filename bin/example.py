e7db1d515296a55a411b8c35fcb32bfc97a77428
Keith Burghardt <keithab@isi.edu>
2025-05-10 21:53:54 -0700

    Update example.py

--- bin/example.py
+++ bin/example.py
@@ -1,7 +1,8 @@
 from hazdet.inference import inference
 from glob import glob
 def main():
-    file = 'test.csv'
+    # We assume that text is in a column "text"
+    file = 'text.csv'
     model_file = list(glob('../*/*.sav'))[0]
     predictions = inference(file,model_file)
     
