3b2c2da2fad1d575636adaacd206dcd4b625332d
Keith Burghardt <keithab@isi.edu>
2025-05-13 20:06:14 -0700

    Update quickstart_inference.py

--- bin/quickstart_inference.py
+++ bin/quickstart_inference.py
@@ -4,6 +4,8 @@ def main():
     # We assume that text is in a column "text"
     filename = 'text.csv'
     model_filename = list(glob('../*/*.sav'))[0]
+    # Output: Pandas dataframe of file in text.csv with additional column called "hazard", 
+    # which lists the hazard confidence for text in each row
     predictions = inference(filename,model_filename)
     
 
