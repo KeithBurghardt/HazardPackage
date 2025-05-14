f7a038bf5a98efe141b5416f71391f2156a648a2
Keith Burghardt <keithab@isi.edu>
2025-05-13 20:08:10 -0700

    Update quickstart_train.py

--- bin/quickstart_train.py
+++ bin/quickstart_train.py
@@ -2,6 +2,9 @@ from hazdet.train import hyperparameter_tune_all_models
 # If you have raw qualtrics data, we have helper functions to clean it
 # e.g., X,y =  create_features(file='Tweet annotation.csv')
 
-# saves best model as 'finalized_model_'+best_model+'.sav'
+# This is your training data
 filename ='../ground_truth_data/TweetGT.csv'
+# saves best model as 'finalized_model_'+best_model+'.sav', 
+# where best model could be NN (feed-forward neural network), RF (random forest), XGB (XGBoost), or SVM (support vector machine)
+# Also saves performance (ROC-AUC) of each candidate model in a pickle file
 hyperparameter_tune_all_models(filename)
